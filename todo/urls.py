b2d12ae8d83e2405ec6e02d953a55f3925b5d050
Leo <routa1082@gmail.com>
2026-02-20 13:49:33 +0100

    feat: add create functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.todo_list, name = "todo_list"),
     path('completed/<int:id>/', views.is_completed, name='is_completed'),
     path('<int:id>/delete/', views.todo_delete, name='todo_delete'),
+    path('create/', views.todo_create, name = 'todo_create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import TodoForm
 from .models import Todo
 # Create your views here.
 def todo_list(request):
@@ -15,4 +17,17 @@ def todo_delete(request, id):
     todo = get_object_or_404(Todo, id = id, owner = request.user)
     todo.delete()
     return redirect('todo_list')
+
+def todo_create(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit = False)
+            todo.owner = request.user
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_create.html', {'form': form})
+
 
