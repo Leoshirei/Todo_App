5ae4ad0a08f13e8d0b21d380852a4b0aa0e26c0e
Leo <routa1082@gmail.com>
2026-02-20 14:23:10 +0100

    feat: add login and logout functionality

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,28 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, get_object_or_404, redirect
-
 from .forms import TodoForm
 from .models import Todo
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+#Login/Logout/Register
+def login_todo(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('todo_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'todo/login.html', {'form': form})
+
+def logout_todo(request):
+    logout(request)
+    return redirect('login')
+
+#Manipulate ToDo
 def todo_list(request):
     todos = Todo.objects.filter(owner = request.user)
     return render(request, "todo/todo_list.html", {"todos": todos})
