60ca425e51676e2141676c35c1e57756bb9074a6
Leo <routa1082@gmail.com>
2026-02-20 12:52:54 +0100

    feat: add todo show functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.filter(owner = request.user)
+    return render(request, "todo/todo_list.html", {"todos": todos})
